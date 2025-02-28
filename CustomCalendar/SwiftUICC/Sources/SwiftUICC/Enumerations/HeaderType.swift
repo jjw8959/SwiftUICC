d270e223a66e02a08ebb58433493746ebe347825
JaeWoongJeong <jjw8959@naver.com>
2025-02-28 21:23:53 +0900

    feat: 헤더타입 일단은 완성해놓음(전에 완성해두었으나 사용 자체를 고려중)

---
+++ CustomCalendar/SwiftUICC/Sources/SwiftUICC/Enumerations/HeaderType.swift
@@ -0,0 +1,19 @@
+//
+//  HeaderType.swift
+//  SwiftUICC
+//
+//  Created by woong on 1/31/25.
+//
+
+import Foundation
+
+/// - Infos for Header
+/// - 헤더에 표시될 정보 유형
+enum HeaderType {
+    /// - show Month with Year
+    case yearMonth
+    
+    /// - show only Month
+    case month
+}
+
